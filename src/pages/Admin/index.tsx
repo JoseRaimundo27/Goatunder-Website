85e3c5365d128e3fe4187bfb3d922d8affb19c76
José PC <joseraimundoalves@hotmail.com>
2024-06-21 11:41:40 -0300

    usando login

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,3 +1,4 @@
+import AdminDashboard from '../../components/AdminDashboard'
 import { useRedirectIfNotAuthenticated } from '../../hooks/useRedirectIfNotAuthenticated'
 import MainLayout from '../../layouts/MainLayout/MainLayout'
 
@@ -5,7 +6,7 @@ const Admin = () => {
   useRedirectIfNotAuthenticated('/login')
   return (
     <MainLayout>
-      <h1 style={{'color': 'white', 'marginTop' : '6rem', "textAlign" : 'center', 'fontSize' : '5rem'}}> Admin session</h1>
+      <h1 style={{'color': 'white', 'marginTop' : '6rem', "textAlign" : 'center', 'fontSize' : '5rem'}}> <AdminDashboard/></h1>
     </MainLayout>
   )
 }
